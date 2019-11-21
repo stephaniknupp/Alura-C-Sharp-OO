ef86e6feb235a6a656fb1cb93f9c6d8cb551eec5
Stephani Knupp <stephaniknupp@hotmail.com>
2019-11-21 15:51:12 -0300

    first commit

---
+++ ByteBank/01-ByteBank/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDaGabriella = new ContaCorrente();
+            contaDaGabriella.titular = "Gabriella";
+            contaDaGabriella.agencia = 863;
+            contaDaGabriella.numero = 863452;
+            contaDaGabriella.saldo = 100;
+
+            Console.WriteLine(contaDaGabriella.titular);
+            Console.WriteLine("Agência: " + contaDaGabriella.agencia);
+            Console.WriteLine("Número: " +contaDaGabriella.numero);
+            Console.WriteLine("Saldo: " +contaDaGabriella.saldo);
+
+            contaDaGabriella.saldo += 200;
+
+            Console.WriteLine("Saldo: " + contaDaGabriella.saldo);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ByteBank/05-ByteBank/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Cliente gabriela = new Cliente();
+
+           // gabriela.nome = "Gabriela";
+           // gabriela.profissao = "Desenvolvedora C#";
+           //gabriela.cpf = "434.562.878-20";
+
+            ContaCorrente conta = new ContaCorrente();
+
+            // conta.titular = gabriela;
+            //conta.titular = new Cliente();
+            // conta.titular.nome = "Gabirela Costa";
+            conta.titular.cpf = "434.562.878-20";
+            conta.titular.profissao = "Desenvolvedora C#";
+            conta.numero = 5634527;
+            conta.agencia = 563;
+            conta.saldo = 500;
+
+            conta.titular.nome = "Gabirela Costa";
+
+            // Console.WriteLine(gabriela.nome);
+            Console.WriteLine(conta.titular.nome);
+            Console.WriteLine(conta.titular.cpf);
+            Console.WriteLine(conta.titular.profissao);
+            Console.ReadLine();
+        }
+    }
+}
